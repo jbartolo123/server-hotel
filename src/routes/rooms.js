4e82bd0e41fc90a7bb521b8ea1b2bffda78c6308
JBartoloHuaman <jesusbartolojbh@gmail.com>
2024-07-24 00:59:01 -0500

    add put rooms

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -32,6 +32,22 @@ roomRoutes.post("/create", async (req, res) => {
   }
 });
 
+roomRoutes.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { image } = req.body
+    const room = Room.findByPk(id)
+    if (room) {
+      room.image = image;
+      await room.save();
+      res.status(200).json({hecho:true});
+    }
+    console.log("no se encontro");
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+})
+
 roomRoutes.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
